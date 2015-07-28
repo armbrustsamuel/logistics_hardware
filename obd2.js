1659e237f4266e254cdfe92a26d743f5d56cc3b6
i843890 <samuel.armbrust@sap.com>
2015-07-28 00:12:40 -0300

    new function OBDII

--- obd2.js
+++ obd2.js
@@ -1,8 +1,19 @@
 var events = require("events"),
   util = require("util");
 
+var currentDate = new Date();
+
+//Initializing OBD
+var OBDReader = require('bluetooth-obd');
+var btOBDReader = new OBDReader();
+
 //Sensors data structure
 var data = {
+	Id: '',
+	engineRPM: '',
+	speed: '',
+	fuelLevel: '',
+	temp: ''
 }
 
 //Constructor
@@ -16,6 +27,46 @@ function OBD2(board) {
     }
   });
 }
+
+//Constructor (second option)
+btOBDReader.on('connected', function () {
+	this.addPoller("vss");
+	this.addPoller("rpm");
+	this.addPoller("temp");
+	this.startPolling(3000);
+});
+
+//Error handling
+btOBDReader.on('error', function (error) {
+	console.log(error);
+});
+
+//Data received
+btOBDReader.on('dataReceived', function (dataR){
+	
+	console.log(currentDate.getTime());
+
+	if(this.requestValueByName("rpm") != null) {
+		data.engineRPM = dataR;
+	}
+
+	if(this.requestValueByName("vss") != null) {
+		data.speed = dataR;
+	}
+
+	if(this.requestValueByName("temp") != null) {
+		data.temp = dataR;
+	}
+
+	if(this.requestValueByName("fuelsys") != null) {
+		data.fuelLevel = dataR;
+	}
+	console.log(dataR);
+	//console.log("Data received");
+	//data = dataR;
+});
+
+btOBDReader.autoconnect('obd');
 
 util.inherits(OBD2, events.EventEmitter);
 module.exports = OBD2;
